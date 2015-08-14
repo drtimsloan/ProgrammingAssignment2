1041762f52a86ac3f7a1e65bcdbb8e88f8868742
Tim Sloan <drtimsloan@gmail.com>
2015-08-14 21:19:05 +0100

    Added the functions to cachematrix which can calculate, cache and recall matrix inversions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions make use of lexical scoping in R to store the inverse of a matrix
+## and recall it without recalculating if already cached. Begin by calling makeCacheMatrix
+## on the matrix to be inverted and assigning it to an object, e.g. x, then call cacheSolve 
+## on the assigned object. A new value for the matrix can be assigned by calling x$set()
 
-## Write a short comment describing this function
+## makeCacheMatrix is a list of four functions to set and recall the values of the matrix x
+## and inverse matrix i
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+      x <<- y
+      i <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) i <<- inv
+    getinv <- function() i
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function checks to see if the inverse of the matrix x is already
+## cached and calculates it if not
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinv()
+    if(!is.null(i)) {
+      message("getting cached data")
+      return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinv(i)
+    i
 }
